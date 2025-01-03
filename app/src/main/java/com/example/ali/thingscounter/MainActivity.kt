ae2a3a717ee0415a38efbf013e7386ec4d3cb955
Ali <biz.alihabibi@gmail.com>
2025-01-04 02:02:52 +0330

    Request Camera Permission (Run-Time Permission)

--- app/src/main/java/com/example/ali/thingscounter/MainActivity.kt
+++ app/src/main/java/com/example/ali/thingscounter/MainActivity.kt
@@ -1,14 +1,38 @@
 package com.example.ali.thingscounter
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.ali.thingscounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var isCameraPermissionGranted = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
+
+    private fun requestCameraPermission(): Boolean {
+        val permission: String = Manifest.permission.CAMERA
+        val cameraPermission = ContextCompat.checkSelfPermission(this, permission)
+        if (cameraPermission == PackageManager.PERMISSION_GRANTED)
+            return true
+        else {
+            requestPermissionLauncher.launch(permission)
+            return isCameraPermissionGranted
+        }
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            isCameraPermissionGranted = when (isGranted) {
+                true -> true
+                false -> false
+            }
+        }
 }
