ba76226096dfaf82674b09e5c2d4ad7f4bc7aa77
Ali <biz.alihabibi@gmail.com>
2025-01-04 15:55:35 +0330

    Make App Full Screen

--- app/src/main/java/com/example/ali/thingscounter/MainActivity.kt
+++ app/src/main/java/com/example/ali/thingscounter/MainActivity.kt
@@ -7,12 +7,19 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.example.ali.thingscounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var isCameraPermissionGranted = false
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.getInsetsController(window, window.decorView).apply {
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            hide(WindowInsetsCompat.Type.systemBars())
+        }
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
