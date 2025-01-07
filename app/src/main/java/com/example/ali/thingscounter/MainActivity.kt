a43aee0155972f51ccf54aa5c69e734b751d32e7
Ali <biz.alihabibi@gmail.com>
2025-01-07 17:21:24 +0330

    Build Up test UI

--- app/src/main/java/com/example/ali/thingscounter/MainActivity.kt
+++ app/src/main/java/com/example/ali/thingscounter/MainActivity.kt
@@ -6,15 +6,18 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.ImageCapture
 import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.example.ali.thingscounter.databinding.ActivityMainBinding
+import java.util.concurrent.ExecutorService
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var isCameraPermissionGranted = false
+    private var imageCapture: ImageCapture? = null
+    private lateinit var cameraExecutor: ExecutorService
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.getInsetsController(window, window.decorView).apply {
             systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
@@ -23,22 +26,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val permission: String = Manifest.permission.CAMERA
-        val cameraPermission =
-            ContextCompat.checkSelfPermission(this, permission)
-        if (cameraPermission == PackageManager.PERMISSION_DENIED) {
-            requestPermissionLauncher.launch(permission)
-        }
+
     }
 
-    private val requestPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-            when (isGranted) {
-                false -> {
-                    val message = "App needs camera Permission to work properly"
-                    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-                }
-                true -> isCameraPermissionGranted = true
-            }
-        }
 }
