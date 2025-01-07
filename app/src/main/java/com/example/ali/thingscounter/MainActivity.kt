6b1a0cd36b0b757504f9a5e35438550edb1e9fb1
Ali <biz.alihabibi@gmail.com>
2025-01-07 17:37:27 +0330

    Basic Implementation of CameraX In MainActivity

--- app/src/main/java/com/example/ali/thingscounter/MainActivity.kt
+++ app/src/main/java/com/example/ali/thingscounter/MainActivity.kt
@@ -2,9 +2,8 @@ package com.example.ali.thingscounter
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.ImageCapture
 import androidx.core.content.ContextCompat
@@ -13,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.example.ali.thingscounter.databinding.ActivityMainBinding
 import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -26,7 +26,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        // Request Camera permission
+        if (allPermissionGranted())
+            startCamera()
+        else
+            requestPermissions()
 
+        binding.imageCaptureButton.setOnClickListener { captureImage() }
+        cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
+    private fun captureImage() {}
+
+    private fun startCamera() {}
+
+    private fun requestPermissions() {}
+
+    private fun allPermissionGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext,
+            it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private val REQUIRED_PERMISSIONS: Array<String> =
+            mutableListOf(Manifest.permission.CAMERA).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+    }
 }
